5e435704cf9ef286d936dc9aeddef7fdf1465fdf
Ayush Rawal <ayushrawal12@gmail.com>
2018-05-17 22:06:45 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ export class InFrontOfYou extends React.Component {
   render() {
     return (
       <div>
-        <p>You shouldn&quot;t look too far.</p>
+        <p>You shouldn&#x27;t look too far.</p>
         <p>Sometimes, the solution is right in front of you.</p>
       </div>
     )
